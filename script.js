ebedb04f67fcfc2bc95db748f4b8e69449f8da90
Manav <manav10799@gmail.com>
2022-11-10 00:37:09 +0530

    Added New Warning

---
+++ script.js
@@ -0,0 +1,6 @@
+let timeLeft= 3;
+
+setTimeout(()=>{
+    document.querySelector('.deprecated-body').style.display= "block";
+    document.querySelector('.main-body').style.display= "none";
+},3000)
